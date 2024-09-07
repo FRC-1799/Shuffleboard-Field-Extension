bd995c5c821cbbb81049a7efab1c69ed7cc3dc45
oof15642 <59772241+Fooot-Code@users.noreply.github.com>
2024-09-07 13:11:44 -0600

    Moved here

---
+++ example-plugins/Field/main/java/com/example/simplewidget/SimpleWidgetExamplePlugin.java
@@ -0,0 +1,22 @@
+package com.example.simplewidget;
+
+import edu.wpi.first.shuffleboard.api.data.DataType;
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.widget.ComponentType;
+import edu.wpi.first.shuffleboard.api.widget.WidgetType;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An example plugin that provides a custom data type (a 2D point) and a simple widget for viewing such data.
+ */
+@Description(
+    group = "com.example",
+    name = "SimpleWidgetExample",
+    version = "2019.1.1",
+    summary = "An example plugin that provides a simple data type and a widget for viewing it"
+)
+public final class SimpleWidgetExamplePlugin extends Plugin {
+}
